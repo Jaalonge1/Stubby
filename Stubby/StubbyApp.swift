1478d764a0469882e9db286e54683ac6861848e5
jaalonge1 <josh.alonge@digital-clarity.io>
2022-05-09 13:52:56 -0400

    Initial Commit

---
+++ Stubby/StubbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StubbyApp.swift
+//  Stubby
+//
+//  Created by Josh Alonge on 5/9/22.
+//
+
+import SwiftUI
+
+@main
+struct StubbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
